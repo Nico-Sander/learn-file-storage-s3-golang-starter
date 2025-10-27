ab0646ca63785fb99c260a1bffdb8e2012114772
Nico-Sander <sandernico512@gmail.com>
2025-10-27 17:36:42 +0100

    Ch1 L5

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,65 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	// Parse the form data
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Form", err)
+		return
+	}
+
+	// Get the image data
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't get file", err)
+		return
+	}
+
+	// Get the media type
+	mediaType := header.Header.Get("Content-Type")
+
+	// Read the data
+	data, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't read data", err)
+		return
+	}
+
+	// Get the video metadata from the DB
+	dbVideo, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't get video metadata from DB", err)
+		return
+	}
+
+	// Check if video belongs to user
+	if dbVideo.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Video doesn't belong to users", err)
+		return
+	}
+
+	// Create a new Thumbnail to store
+	videoThumbnail := thumbnail{
+		data:      data,
+		mediaType: mediaType,
+	}
+
+	// Store the thumbnail in the global map
+	videoThumbnails[videoID] = videoThumbnail
+
+	// Set the Thumbnail URL
+	url := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoIDString)
+	dbVideo.ThumbnailURL = &url
+	err = cfg.db.UpdateVideo(dbVideo)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video in DB", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, dbVideo)
 }
